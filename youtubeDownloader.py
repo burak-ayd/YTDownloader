c61bd20c664b3cd7ad54b5d6f11d3e0a5850c925
Burak Aydoğan <aydoganburak78@gmail.com>
2022-03-13 16:35:53 +0300

    Gerekli Değişkenler ve Tanımlamalar Yapıldı.

--- youtubeDownloader.py
+++ youtubeDownloader.py
@@ -9,20 +9,42 @@ from requests import get
 from re import findall
 
 class Youtube():
-    def __init__(self):
-        pass
+    def __init__(self,url,video:bool,quality:int,audio:bool=False):
+        self.url=url
+        self.video=video
+        self.audio=audio
+        self.quality=quality
+        self.options = Options()
+        self.options.add_argument("--headless")
+        self.chrome_driver = r"%userprofile%\Documents\Python Projeler\YoutubeImageDownloader\chromedriver.exe"
+        self.driver = webdriver.Chrome(options=self.options,executable_path=self.chrome_driver)
+        self.cwd = path.dirname(path.abspath(__file__))
+        self.config = load(open(self.cwd+"/config.json", "rb"))
+        self.videoData=self.get_video_data(self.url)
+        self.streamData=self.videoData["streamingData"]
+        self.videoDetails=self.videoData["videoDetails"]
+        self.videoTitle=self.videoDetails["title"]
+        self.thumbnail="https://i.ytimg.com/vi/{}/maxresdefault.jpg".format(self.videoDetails["videoId"])
+        
     
     def get_js(self):
-        pass
+        self.base_data = get('https://youtube.com/watch?v=1').text
+        self.js_url = 'https://youtube.com/'+findall(r'"jsUrl":"(.*?)"', self.base_data)[0]
+        self.js_file = get(self.js_url).text
+        self.data = Decipher(self.js_file, process=True).get_full_function()
+        self.config['js'] = self.data
+        open(self.cwd+'/config.json', 'w').write(dumps(self.config))
+        return self.data
     
-    def get_page_source(self):
-        pass
-    
-    def save_video_source(self):
-        pass
-    
-    def load_video_source(self):
-        pass
+    def get_video_data(self,url:str):  
+        self.driver.get(url)
+        self.source=self.driver.find_element_by_xpath('/html/body/script[1]').get_attribute('innerHTML')
+        self.driver.close()
+        x=self.source.find("{")
+        self.source=self.source[x:]
+        x=self.source.find("; var")
+        self.source=self.source[:x]
+        return self.source
     
     def get_download_url(self):
         pass
