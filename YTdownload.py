67c8922dce784723779474289d29be6a0b5d5831
Burak Aydoğan <aydoganburak78@gmail.com>
2022-03-14 00:03:51 +0300

    Class ile bağlantı oluşturuldu

--- YTdownload.py
+++ YTdownload.py
@@ -1,5 +1,7 @@
 import argparse
 import sys
+from youtubeDownloader import Youtube
+
 
 parser = argparse.ArgumentParser(description="A Python script to download youtube videos withhelp of ffmpeg" , epilog="Download restricted videos can't be download")
 parser.add_argument( "-u" , help="Video URL" , metavar='url' , dest="url" , type=str , required=True)
@@ -8,6 +10,7 @@ parser.add_argument( "-q" , help="Video Quality (1080-720-480-360)" , metavar='q
 parser.add_argument( "-a" , help="Download Only Audio" , metavar='audio' , dest="audio" , type=int)
 args = parser.parse_args()
 
-if args.video == None and args.audio == None:
-    print("Lütlen indirmek istediğiniz formatı seçiniz")
-    exit(1)
+
+    
+    
+obj = Youtube(args.url)
