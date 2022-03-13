2aac532d273d1c2dfa049b484416158086e0d48c
Burak Aydoğan <aydoganburak78@gmail.com>
2022-03-13 16:01:34 +0300

    Ana Class yapısı oluşturuldu ve Encrypted Urller için Decoder oluşturuldu.

--- YTdownload.py
+++ YTdownload.py
@@ -4,10 +4,10 @@ import sys
 parser = argparse.ArgumentParser(description="A Python script to download youtube videos withhelp of ffmpeg" , epilog="Download restricted videos can't be download")
 parser.add_argument( "-u" , help="Video URL" , metavar='url' , dest="url" , type=str , required=True)
 parser.add_argument( "-v" , help="Download Video" , metavar='video' , dest="video" , type=int )
-parser.add_argument( "-q" , help="Video (1080-720-480-360)" , metavar='quality' , dest="quality" , type=int )
+parser.add_argument( "-q" , help="Video Quality (1080-720-480-360)" , metavar='quality' , dest="quality" , type=int )
 parser.add_argument( "-a" , help="Download Only Audio" , metavar='audio' , dest="audio" , type=int)
 args = parser.parse_args()
 
-if args.vtag == None and args.atag == None:
+if args.video == None and args.audio == None:
     print("Lütlen indirmek istediğiniz formatı seçiniz")
     exit(1)
---
+++ youtubeDownloader.py
@@ -0,0 +1,31 @@
+from msilib.schema import Class
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from json import load, dumps
+from Decipher import Decipher
+from urllib.parse import unquote
+from os import path
+from requests import get
+from re import findall
+
+class Youtube():
+    def __init__(self):
+        pass
+    
+    def get_js(self):
+        pass
+    
+    def get_page_source(self):
+        pass
+    
+    def save_video_source(self):
+        pass
+    
+    def load_video_source(self):
+        pass
+    
+    def get_download_url(self):
+        pass
+    
+    def save_download_url(self):
+        pass
